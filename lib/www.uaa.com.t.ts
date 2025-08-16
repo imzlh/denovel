ceb01991dfcb3e09187bb084905dd175fd4e07f0
imzlh <2131601562@qq.com>
2025-08-16 22:10:40 +0800

    feat: UAA novel support

---
+++ lib/www.uaa.com.t.ts
@@ -0,0 +1,76 @@
+import { readline } from "../exe.ts";
+import { fetch2, getDocument, getSiteCookie, setRawCookie } from "../main.ts";
+
+// login
+while(true) try{
+    const testDoc = await getDocument('https://www.uaa.com/member/center');
+    const unEl = testDoc.querySelector("body > div.main_box > div.user_box > div.info_box > div.account_box > div");
+    if(!unEl){
+        await fetch2('https://www.uaa.com/');   // get SessionID
+        const rawCache = getSiteCookie('www.uaa.com', '__DN_USPW__');
+        let uname, upass;
+        if(rawCache){
+            console.log('尝试自动登录...');
+            const [un, up] = rawCache.split(',');
+            uname = un, upass = up;
+        }else{
+            console.log('检测到你没有登陆或登陆失败，回车表示放弃');
+            uname = await readline('用户名 > ');
+            upass = await readline('密码 > ');
+        }
+
+        // 验证码
+        const caUrl = 'https://uaa.com/email/captcha';
+        const caRes = await fetch2(caUrl);
+        if(!caRes.ok || !caRes.body) {
+            console.log('验证码获取失败，请重试');
+            await readline('回车继续 ?');
+            continue;
+        }
+        await Deno.writeFile('captcha.jpg', caRes.body);
+        if(Deno.build.os === 'windows'){
+            new Deno.Command('explorer.exe', {
+                args: ['captcha.jpg']
+            }).outputSync();
+        }else{
+            console.log('请查看captcha.jpg并输入验证码');
+        }
+
+        const captcha = await readline('验证码 > ');
+        await fetch2('https://accounts.livechatinc.com/v2/customer/token'); // token
+        Deno.removeSync('captcha.jpg');
+        const res = await fetch2('https://www.uaa.com/login', {
+            method: 'POST',
+            body: new URLSearchParams({
+                loginName: uname,
+                password: upass,
+                code: captcha
+            })
+        }).then(res => res.json());
+        if(res.result == 'fail'){
+            console.log('登录失败，请重试:', res.msg);
+        }else{
+            console.log('登录成功');
+            setRawCookie('uaa.com', `__DN_USPW__=${uname},${upass}`);
+        }
+    }else{
+        console.log('欢迎,', unEl.textContent.trim());
+        break;
+    }
+}catch(e){
+    console.log(e);
+    await readline('回车继续 ?');
+}
+
+export default {
+    title: 'body > div.main_box > div.chapter_box > div.title_box.theme_2 > h2',
+    content: 'body > div.main_box > div.chapter_box > div.article',
+    next_link: 'a.next_chapter',
+
+    // https://www.uaa.com/novel/intro?id=1003703927742533632
+    mainPageLike: /https:\/\/www\.uaa\.com\/novel\/intro\?id=\d+?$/,
+    mainPageTitle: 'body > div.main_box > div.content_box > div.left_box > div.novel_box > div > h1',
+    mainPageCover: 'body > div.main_box > div.content_box > div.left_box > div.novel_box > img',
+    mainPageSummary: 'body > div.main_box > div.content_box > div.left_box > div.detail_box > div.brief_box > div.txt.ellipsis',
+    mainPageFirstChapter: 'body > div.main_box > div.content_box > div.left_box > div.detail_box > a'
+} satisfies TraditionalConfig;
